6652382c5ecef5867ffa380919efe19ca4c69daf
Abdul Haseeb <haseebimran221@gmail.com>
2020-08-29 17:08:32 +0500

    Shoe Store with Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+import { Products} from './components/Product';
+import { ProductDetails } from './components/ProductDetails';
+import { NotFound } from './components/NotFound';
+import { ProductIndex } from './components/ProductIndex';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="products" element={<Products />}>
+          <Route path="/" element={<ProductIndex />}></Route>
+          <Route path=":productID" element={<ProductDetails />}></Route>
+        </Route>
+        <Route path="*"element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from'react';
+
+export const Home =() => {
+    return (
+        <div>
+           <h3>HOME PAGE OF SHOE STORE BY <i>HASEEB IMRAN</i></h3>
+        </div>
+    )
+}
